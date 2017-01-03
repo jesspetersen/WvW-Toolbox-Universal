7e2d0caa88ab6a8488fa23c24c0ea4accb5b20fa
Jess <jessisawesome@hotmail.co.nz>
2017-01-03 16:38:50 +1300

    RankCalc UI working but ugly.

---
+++ WvW Toolbox/WvW_Toolbox/pages/RankCalcPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace WvW_Toolbox.pages
+{
+    public partial class RankCalcPage : ContentPage
+    {
+        public RankCalcPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RankNumberButton_Clicked(object sender, EventArgs e)
+        {
+            if (RankNameSection.IsVisible)
+                RankNameSection.IsVisible = false;
+
+            if (!RankNumberSection.IsVisible)
+                RankNumberSection.IsVisible = true;
+
+            if (RankNameButton.BackgroundColor == Color.Red)
+                RankNameButton.BackgroundColor = Color.Gray;
+
+            if (RankNumberButton.BackgroundColor != Color.Red)
+                RankNumberButton.BackgroundColor = Color.Red;
+        }
+
+        private void RankNameButton_Clicked(object sender, EventArgs e)
+        {
+            if (RankNumberSection.IsVisible)
+                RankNumberSection.IsVisible = false;
+
+            if (!RankNameSection.IsVisible)
+                RankNameSection.IsVisible = true;
+
+            if (RankNumberButton.BackgroundColor == Color.Red)
+                RankNumberButton.BackgroundColor = Color.Gray;
+
+            if (RankNameButton.BackgroundColor != Color.Red)
+                RankNameButton.BackgroundColor = Color.Red;
+        }
+    }
+}
