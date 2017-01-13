264a6defd0405eca354abac58d14c44991c3fa96
Jess <jessisawesome@hotmail.co.nz>
2017-01-13 16:48:32 +1300

    World member display implemented, dynamic world detail view added

--- WvW Toolbox/WvW_Toolbox/pages/MatchMonitorPage.xaml.cs
+++ WvW Toolbox/WvW_Toolbox/pages/MatchMonitorPage.xaml.cs
@@ -35,11 +35,86 @@ namespace WvW_Toolbox.pages
                 BlueWorldGlanceText.Text = "POINTS: " + match.blue.points + " | KDR: " + Math.Round(Convert.ToDouble(match.blue.kills) / Convert.ToDouble(match.blue.deaths), 2);
                 GreenWorldGlanceText.Text = "POINTS: " + match.green.points + " | KDR: " + Math.Round(Convert.ToDouble(match.green.kills) / Convert.ToDouble(match.green.deaths), 2);
 
+                PreparePage(match);
+
             }
             catch
             {
                 App.Current.MainPage.DisplayAlert("Alert", "Unable to access GW2 server for match data. Please check your internet connection, firewall, and chosen server.", "OK");
             }
         }
-	}
+
+        public void PreparePage(objects.Match match)
+        {
+            if (Application.Current.Properties.ContainsKey("Server"))
+            {
+                if (match.red.worldIDs.Contains(Application.Current.Properties["Server"]))
+                    RedExpand();
+
+                if (match.green.worldIDs.Contains(Application.Current.Properties["Server"]))
+                    GreenExpand();
+
+                if (match.blue.worldIDs.Contains(Application.Current.Properties["Server"]))
+                    BlueExpand();
+            }
+
+            for (int i = 0; i <match.red.worldIDs.Length; i++)
+            {
+                RedWorldsLabel.Text += "\n" + utilities.WorldSearchHelper.FindWorldById(Convert.ToInt32(match.red.worldIDs[i])).name;
+            }
+
+            for (int i = 0; i < match.blue.worldIDs.Length; i++)
+            {
+                BlueWorldsLabel.Text += "\n" + utilities.WorldSearchHelper.FindWorldById(Convert.ToInt32(match.blue.worldIDs[i])).name;
+            }
+
+            for (int i = 0; i < match.green.worldIDs.Length; i++)
+            {
+                GreenWorldsLabel.Text += "\n" + utilities.WorldSearchHelper.FindWorldById(Convert.ToInt32(match.green.worldIDs[i])).name;
+            }
+        }
+
+        public void RedExpand()
+        {
+            if (GreenExpandSection.IsVisible)
+                GreenExpandSection.IsVisible = false;
+
+            if (BlueExpandSection.IsVisible)
+                BlueExpandSection.IsVisible = false;
+
+            if (RedExpandSection.IsVisible == false)
+                RedExpandSection.IsVisible = true;
+        }
+
+        public void GreenExpand()
+        {
+            if (BlueExpandSection.IsVisible)
+                BlueExpandSection.IsVisible = false;
+
+            if (RedExpandSection.IsVisible)
+                RedExpandSection.IsVisible = false;
+
+            if (GreenExpandSection.IsVisible == false)
+                GreenExpandSection.IsVisible = true;
+        }
+
+        public void BlueExpand()
+        {
+            if (RedExpandSection.IsVisible)
+                RedExpandSection.IsVisible = false;
+
+            if (GreenExpandSection.IsVisible)
+                GreenExpandSection.IsVisible = false;
+
+            if (BlueExpandSection.IsVisible == false)
+                BlueExpandSection.IsVisible = true;
+        }
+
+        private void RedWorldButton_Clicked(object sender, EventArgs e) { RedExpand(); }
+
+        private void GreenWorldButton_Clicked(object sender, EventArgs e) { GreenExpand(); }
+
+        private void BlueWorldButton_Clicked(object sender, EventArgs e) { BlueExpand(); }
+
+    }
 }
